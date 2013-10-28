c54b1bf6066775be2b22dc730ad719b425ec4620
Dave Parfitt <dparfitt@basho.com>
2013-10-28 09:47:05 -0400

    script! yay

--- util/gen_pages.sh
+++ util/gen_pages.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 ## run this script right from ./utils
-cd .. && scons docs
-mkdir ../riakc_pages
-cp -r ./docs/html/* ../riakc_pages
+(cd .. && scons docs)
+   #cp -r ../docs/html/* ../riakc_pages
+git clone git@github.com:/basho/riak-c-client.git pages
+cd pages
 git checkout gh-pages
-cp -r ../riakc_pages/* ./
+cp -r ../../riakc_pages/* ./
+git add .
 git commit -a -m "Generated gh_pages"
 git push origin gh_pages
 
