8cddcffe9cee92efc98e902d0cf1d023f2b7e843
Dave Parfitt <dparfitt@basho.com>
2013-10-28 09:35:53 -0400

    Generated gh_pages

--- util/gen_pages.sh
+++ util/gen_pages.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
+## run this script right from ./utils
 cd .. && scons docs
 mkdir ../riakc_pages
 cp -r ./docs/html/* ../riakc_pages
+git checkout gh-pages
+cp -r ../riakc_pages/* ./
+git commit -a -m "Generated gh_pages"
 
