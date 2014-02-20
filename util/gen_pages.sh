12639b1c47aa349518ac03ac3ac59568869ecec3
Dave Parfitt <dparfitt@basho.com>
2014-02-20 16:57:22 -0500

    Generated gh_pages

--- util/gen_pages.sh
+++ util/gen_pages.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
+cwd=${PWD##*/}
+if [ "$cwd" != "util" ]
+then
+  echo "Please run gen_pages.sh from the ./util directory"
+  exit 1
+fi
+
 ## run this script right from ./utils
-(cd .. && scons docs)
+(cd .. && ./autogen.sh && doxygen)
 git clone git@github.com:/basho/riak-c-client.git pages
 cd pages
 git checkout gh-pages
