da321e55ac1694476f796239d7ac99d2efb5e78d
Dave Parfitt <dparfitt@basho.com>
2014-02-20 17:00:26 -0500

    cleanup gen_pages.sh

--- util/gen_pages.sh
+++ util/gen_pages.sh
@@ -8,11 +8,12 @@ fi
 
 ## run this script right from ./utils
 (cd .. && ./autogen.sh && doxygen)
+rm -rf ./pages
 git clone git@github.com:/basho/riak-c-client.git pages
 cd pages
 git checkout gh-pages
 cp -r ../../docs/html/* ./
 git add .
 git commit -a -m "Generated gh_pages"
-#git push origin gh-pages
+git push origin gh-pages
 
